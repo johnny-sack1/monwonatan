0942a871d71f71be097aa5b6208d0b4f4af7f273
WojTechM <wojciech.m1997@gmail.com>
2018-05-23 19:05:52 +0200

    Method loadUser now throws SQLException

--- queststore/src/main/java/com/codecool/queststore/DAO/UserDAO.java
+++ queststore/src/main/java/com/codecool/queststore/DAO/UserDAO.java
@@ -3,10 +3,11 @@ package com.codecool.queststore.DAO;
 import com.codecool.queststore.DatabaseConnection.SQLQueryHandler;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public abstract class UserDAO {
 
-    public abstract ResultSet loadUser(String login);
+    public abstract ResultSet loadUser(String login) throws SQLException;
     public void executeQuery(String query) {
         SQLQueryHandler.getInstance().executeQuery(query);
     }
