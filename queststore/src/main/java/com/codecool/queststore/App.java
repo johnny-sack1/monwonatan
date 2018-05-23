6b0b5499642a712b319a7c3189ccc38f3fd2368c
johnny-sack1 <jonymaka@gmail.com>
2018-05-23 17:11:19 +0200

    Add testing code for createEntity in App

--- queststore/src/main/java/com/codecool/queststore/App.java
+++ queststore/src/main/java/com/codecool/queststore/App.java
@@ -1,5 +1,6 @@
 package com.codecool.queststore;
 
+import com.codecool.queststore.DAO.ArtifactDAO;
 import com.codecool.queststore.DAO.IStoreDAO;
 import com.codecool.queststore.DAO.QuestDAO;
 
@@ -7,9 +8,11 @@ import java.sql.SQLException;
 
 public class App {
     public static void main( String args[] ) {
-        IStoreDAO dao = new QuestDAO();
+        IStoreDAO questDAO = new QuestDAO();
+        IStoreDAO artifactDAO = new ArtifactDAO();
         try {
-            dao.createEntity("quest", "new quest", 20);
+            questDAO.createEntity("quest", "new quest", 20);
+            artifactDAO.createEntity("artifact", "new artifact", 19);
         }
         catch (SQLException e) {
             e.printStackTrace();
