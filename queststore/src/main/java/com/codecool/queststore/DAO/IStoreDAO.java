cfd3988ea4917badfeed39dcffe9262543dee048
johnny-sack1 <jonymaka@gmail.com>
2018-05-23 14:48:04 +0200

    Add createEntity() method in IStoreDAO

--- queststore/src/main/java/com/codecool/queststore/DAO/IStoreDAO.java
+++ queststore/src/main/java/com/codecool/queststore/DAO/IStoreDAO.java
@@ -1,11 +1,13 @@
 package com.codecool.queststore.DAO;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public interface IStoreDAO {
-    ResultSet loadEntity(int id);
-    ResultSet loadEntity(String name);
-    void updateEntity(int id, List<String> newData);
-    void updateEntity(String name, List<String> newData);
+//    ResultSet loadEntity(int id) throws SQLException;
+//    ResultSet loadEntity(String name) throws SQLException;
+    void createEntity(String name, String description, int value) throws SQLException;
+//    void updateEntity(int id, List<String> newData) throws SQLException;
+//    void updateEntity(String name, List<String> newData) throws SQLException;
 }
