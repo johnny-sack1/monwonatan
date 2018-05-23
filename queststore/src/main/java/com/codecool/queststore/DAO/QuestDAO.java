4f90efae5512834b0a156f4c81a3ae843d4b173f
reggae-krk <monika.gorecka.agh@gmail.com>
2018-05-23 19:53:14 +0200

    Add updateEntity method to QuestDAO file

--- queststore/src/main/java/com/codecool/queststore/DAO/QuestDAO.java
+++ queststore/src/main/java/com/codecool/queststore/DAO/QuestDAO.java
@@ -22,11 +22,6 @@ public class QuestDAO implements IStoreDAO {
 //        return null;
 //    }
 
-//    @Override
-//    public void updateEntity throws SQLException (int id, List<String> newData) {
-//
-//    }
-
     @Override
     public void createEntity(String name, String description, int value) throws SQLException {
         String query = "INSERT INTO quest (name, description, value) " +
@@ -38,6 +33,18 @@ public class QuestDAO implements IStoreDAO {
         statement.setInt(3, value);
         SQLQueryHandler.getInstance().executeQuery(statement.toString());
 
+    }
+
+    public void updateEntity (int id, List<String> newData) throws SQLException {
+        String query = "UPDATE quest SET name = ?, description = ?, value = ? " +
+                "WHERE id = ?";
+        PreparedStatement statement = c.prepareStatement(query);
+
+        statement.setString(1, newData.get(0));
+        statement.setString(2, newData.get(1));
+        statement.setInt(3, Integer.valueOf(newData.get(2)));
+        statement.setInt(4, id);
+        SQLQueryHandler.getInstance().executeQuery(statement.toString());
     }
 
 //    @Override
