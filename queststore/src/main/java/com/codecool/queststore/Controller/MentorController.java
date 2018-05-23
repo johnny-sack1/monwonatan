076adc3dc748b35a054e045396b190b9456e3582
reggae-krk <monika.gorecka.agh@gmail.com>
2018-05-23 15:22:55 +0200

    Add getStudentSummary method to MentorController file

--- queststore/src/main/java/com/codecool/queststore/Controller/MentorController.java
+++ queststore/src/main/java/com/codecool/queststore/Controller/MentorController.java
@@ -1,14 +1,17 @@
 package com.codecool.queststore.Controller;
 
+import com.codecool.queststore.Model.Mentor;
+import com.codecool.queststore.Model.Student;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
 public class MentorController {
 
-//    private Mentor currentUser;
-//    public MentorDAO mentorDAO;
-//    public StudentDAO studentDAO;
+    private Mentor currentUser;
+    public MentorDAO mentorDAO;
+    public StudentDAO studentDAO;
 
     public void createStudent(List<String> studentData) throws SQLException {
 
@@ -34,7 +37,30 @@ public class MentorController {
 
     }
 
-    public List<ResultSet> getStudentSummary(String studentLogin) {
+    public Student getStudentSummary(String studentLogin) {
+
+        ResultSet resultSet = studentDAO.loadUser(studentLogin);
+        Student currentStudent;
+
+        try {
+
+            while (resultSet.next()) {
+
+                String login =  resultSet.getString(EColumnNumber.LOGIN.indexForDatabase()).toLowerCase();
+                String first_name = resultSet.getString(EColumnNumber.FIRST_NAME.indexForDatabase()).toLowerCase();
+                String last_name = resultSet.getString(EColumnNumber.LAST_NAME.indexForDatabase()).toLowerCase();
+                int classroom_id = resultSet.getInt(EColumnNumber.CLASSROOM.indexForDatabase());
+                String type = resultSet.getString(EColumnNumber.TYPE.indexForDatabase()).toLowerCase();
+                int coins = resultSet.getInt(EColumnNumber.COINS.indexForDatabase());
+
+                currentStudent = new Student(login, first_name, last_name, classroom_id, type, coins);
+                return currentStudent;
+            }
+        }
+        catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+
         return null;
     }
 }
