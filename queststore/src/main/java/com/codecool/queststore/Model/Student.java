d26dda79f41caa33ab5a154c8eae5f88e50c1bed
reggae-krk <monika.gorecka.agh@gmail.com>
2018-05-24 09:44:15 +0200

    Add constructor in Student class

--- queststore/src/main/java/com/codecool/queststore/Model/Student.java
+++ queststore/src/main/java/com/codecool/queststore/Model/Student.java
@@ -4,6 +4,13 @@ public class Student extends User {
 
     private int coolcoins;
 
+    public Student(String firstName, String lastName, String login, String password,
+                   int classRoomID, String userType, int coolcoins) {
+
+        super(firstName, lastName, login, password, classRoomID, userType);
+        this.coolcoins = coolcoins;
+    }
+
     public int getCoolcoins() {
         return this.coolcoins;
     }
