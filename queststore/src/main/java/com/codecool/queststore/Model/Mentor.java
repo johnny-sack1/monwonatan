2c56e2a946dc2fb7f44d5d05b30fab822794f477
reggae-krk <monika.gorecka.agh@gmail.com>
2018-05-24 09:43:36 +0200

    Add constructor in Mentor class

--- queststore/src/main/java/com/codecool/queststore/Model/Mentor.java
+++ queststore/src/main/java/com/codecool/queststore/Model/Mentor.java
@@ -1,9 +1,18 @@
 package com.codecool.queststore.Model;
 
-public class Mentor {
+public class Mentor extends  User {
 
     private String email;
     private String address;
+
+
+    public Mentor(String firstName, String lastName, String login, String password,
+                  int classRoomID, String userType, String email, String address) {
+
+        super(firstName, lastName, login, password, classRoomID, userType);
+        this.email = email;
+        this.address = address;
+    }
 
     public String getEmail() {
         return this.email;
