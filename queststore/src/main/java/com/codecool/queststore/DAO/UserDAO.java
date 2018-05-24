418e5213c0f73e05efa7a02bcec9a1ff9c587c09
johnny-sack1 <jonymaka@gmail.com>
2018-05-24 09:31:40 +0200

    Restore executeQuery() method's return type to void in UserDAO interface

--- queststore/src/main/java/com/codecool/queststore/DAO/UserDAO.java
+++ queststore/src/main/java/com/codecool/queststore/DAO/UserDAO.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public abstract class UserDAO {
 
     public abstract ResultSet loadUser(String login) throws SQLException;
-    ResultSet executeQuery(String query) {
-        return SQLQueryHandler.getInstance().executeQuery(query);
+    void executeQuery(String query) {
+        SQLQueryHandler.getInstance().executeQuery(query);
     }
 }
