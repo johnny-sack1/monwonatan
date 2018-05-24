4563df9342a41e2e3651310f828f2d1e7e50e837
reggae-krk <monika.gorecka.agh@gmail.com>
2018-05-24 17:15:46 +0200

    Add updateClassroom method

--- queststore/src/main/java/com/codecool/queststore/DAO/ClassroomDAO.java
+++ queststore/src/main/java/com/codecool/queststore/DAO/ClassroomDAO.java
@@ -27,5 +27,24 @@ public class ClassroomDAO {
         }
     }
 
+    public boolean updateClassroom(int id, List<String> newData) {
 
+        try {
+            Connection c = SQLQueryHandler.getInstance().getConnection();
+            String query = "UPDATE classroom " +
+                    "SET name = ?, description = ? " +
+                    "WHERE classroom_id = ?;";
+            PreparedStatement statement = c.prepareStatement(query);
+
+            statement.setString(1, newData.get(0));
+            statement.setString(2, newData.get(1));
+            statement.setInt(3, id);
+            SQLQueryHandler.getInstance().executeQuery(statement.toString());
+
+            return true;
+        }
+        catch (SQLException e) {
+            return false;
+        }
+    }
 }
