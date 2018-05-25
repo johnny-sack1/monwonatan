d5ff77867b54e8d4007318d47d8fc07894d59322
WojTechM <wojciech.m1997@gmail.com>
2018-05-25 11:22:01 +0200

    Update imports and try-catch statements in LoginProcessHandler class

--- queststore/src/main/java/com/codecool/queststore/Controller/LoginProcessHandler.java
+++ queststore/src/main/java/com/codecool/queststore/Controller/LoginProcessHandler.java
@@ -1,19 +1,27 @@
 package com.codecool.queststore.Controller;
 
+import com.codecool.queststore.DAO.LoginDAO;
+
+import java.sql.SQLException;
+
 public class LoginProcessHandler {
-//
-//    private LoginDAO loginDAO;
-//
-//    public LoginProcessHandler() {
-//        this.loginDAO = new LoginDAO();
-//    }
-//
-//    public String loginProcess(String login, String password) {
-//        PasswordManager passwordManager = new PasswordManager();
-//        String hashedPassword = loginDAO.getPasswordBy(login);
-//        if (passwordManager.isValidInput(password, hashedPassword)) {
-//            return loginDAO.getTypeBy(login);
-//        }
-//        return "invalid password";
-//    }
+
+    private LoginDAO loginDAO;
+
+    public LoginProcessHandler() {
+        this.loginDAO = new LoginDAO();
+    }
+
+    public String loginProcess(String login, String password) {
+        PasswordManager passwordManager = new PasswordManager();
+        try {
+            String hashedPassword = loginDAO.getPasswordBy(login);
+            if (passwordManager.isValidInput(password, hashedPassword)) {
+                return loginDAO.getTypeBy(login);
+            }
+        } catch (SQLException e) {
+            return "invalid password";
+        }
+        return null;
+    }
 }
