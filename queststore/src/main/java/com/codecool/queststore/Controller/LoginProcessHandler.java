6c15d717e88a354c448466a5604bc27192428a46
reggae-krk <35046203+reggae-krk@users.noreply.github.com>
2018-06-04 13:48:54 +0200

    Password menager (#20)
    * Change name of validate method
    
    * Add PasswordMenager file

--- queststore/src/main/java/com/codecool/queststore/Controller/LoginProcessHandler.java
+++ queststore/src/main/java/com/codecool/queststore/Controller/LoginProcessHandler.java
@@ -1,7 +1,9 @@
 package com.codecool.queststore.Controller;
 
 import com.codecool.queststore.DAO.LoginDAO;
-
+import com.codecool.queststore.PasswordManager;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.sql.SQLException;
 
 public class LoginProcessHandler {
@@ -12,11 +14,11 @@ public class LoginProcessHandler {
         this.loginDAO = new LoginDAO();
     }
 
-    public String loginProcess(String login, String password) {
+    public String loginProcess(String login, String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
         PasswordManager passwordManager = new PasswordManager();
         try {
             String hashedPassword = loginDAO.getPasswordBy(login);
-            if (passwordManager.isValidInput(password, hashedPassword)) {
+            if (passwordManager.validatePassword(password, hashedPassword)) {
                 return loginDAO.getTypeBy(login);
             }
         } catch (SQLException e) {
