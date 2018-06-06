867046fd61dca80b1f5254d38cc8fa1581c20d13
WojTechM <wojciech.m1997@gmail.com>
2018-06-06 10:52:06 +0200

    Submit button now triggers on keypress ('ENTER')

--- queststore/src/main/java/com/codecool/queststore/frontend/js/input-validator.js
+++ queststore/src/main/java/com/codecool/queststore/frontend/js/input-validator.js
@@ -3,7 +3,7 @@ var events = ["keydown", "keyup", "keypressed"];
 for (i = 0; i < input.length; i++) {
     for (j = 0; j < events.length; j++) {
         input[i].addEventListener(events[j], function (event) {
-            var inputs = document.getElementsByClassName("input");
+            var inputs = document.getElementsByTagName("input");
             for (i = 0; i < inputs.length; i++) {
                 if (!inputs[i].value) {
                     document.getElementById("submit-btn").disabled = true;
@@ -11,6 +11,11 @@ for (i = 0; i < input.length; i++) {
                 }
             }
             document.getElementById("submit-btn").disabled = false;
+
+            // If ENTER pressed
+            if (event.keyCode === 13) {
+                document.getElementById("submit-btn").click();
+            }
         });
     }
 }
