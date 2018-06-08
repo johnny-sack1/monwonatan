a8fc5f0bbe1434f2ad55658bfb897e0d45189137
WojTechM <wojciech.m1997@gmail.com>
2018-06-08 09:44:00 +0200

    Add password validation in 'student-editor.js' script

--- queststore/src/main/java/com/codecool/queststore/frontend/js/student-editor.js
+++ queststore/src/main/java/com/codecool/queststore/frontend/js/student-editor.js
@@ -14,7 +14,7 @@ function disableButtonOnEmpty() {
 
     for (var i = 1; i < table.rows.length; i++) {
         var cells_ = table.rows.item(i).cells;
-        if (noEmptyFields(cells_)) {
+        if (validateCells(cells_)) {
             cells_.item(BUTTON_INDEX).getElementsByTagName("button")[0].disabled = false;
         } else {
             cells_.item(BUTTON_INDEX).getElementsByTagName("button")[0].disabled = true;
@@ -25,10 +25,43 @@ function disableButtonOnEmpty() {
 function noEmptyFields(cells_) {
     var FIELDS_TO_SKIP = 1;
     var REQUIRED_FIELDS = 4;
-    for (i = 1; i < 5; i++) {
-        if(cells_.item(i).getElementsByTagName("input")[0].value == "") {
+    var input;
+    for (i = FIELDS_TO_SKIP; i < REQUIRED_FIELDS + FIELDS_TO_SKIP; i++) {
+        input = cells_.item(i).getElementsByTagName("input")[0];
+        if (input.type == "password") {
+            continue;
+        }
+        if(input.value == "") {
             return false;
         }
     }
     return true;
+}
+
+function areValidPasswords(cells_) {
+    var PASS_INDEX = 3;
+    var PASS_2_INDEX = 4;
+
+    var pass1 = cells_.item(PASS_INDEX).getElementsByTagName("input")[0].value;
+    var pass2 = cells_.item(PASS_2_INDEX).getElementsByTagName("input")[0].value;
+
+    console.log(pass1 + " " + pass2);
+
+    if (/^$/.test(pass1) && /^$/.test(pass2)){
+        return true;
+    }
+
+    if(pass1 == pass2) {
+        return pass1.length >= 8;
+    }
+
+    return false;
+}
+
+function validateCells(cells_) {
+    if (noEmptyFields(cells_)) {
+        console.log("no empty");
+        return areValidPasswords(cells_);
+    }
+    return false;
 }
