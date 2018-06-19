fa0ca91d2d5b3826ed4491785fb3df927d2f1612
WojTechM <wojciech.m1997@gmail.com>
2018-06-19 10:05:39 +0200

    Create methods used to redirect and send template responses to browser

--- queststore/src/main/java/com/codecool/queststore/backend/webControllers/AbstractHandler.java
+++ queststore/src/main/java/com/codecool/queststore/backend/webControllers/AbstractHandler.java
@@ -1,5 +1,52 @@
 package com.codecool.queststore.backend.webControllers;
 
+import com.codecool.queststore.backend.session.SessionIdContainer;
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
 public abstract class AbstractHandler {
-    
+    private SessionIdContainer sessionIdContainer;
+
+    public AbstractHandler() {
+        this.sessionIdContainer = SessionIdContainer.getSessionIdContainer();
+    }
+
+    public SessionIdContainer getSessionIdContainer() {
+        return sessionIdContainer;
+    }
+
+    public void redirectToLocation(HttpExchange exchange, String location) {
+        Headers responseHeaders = exchange.getResponseHeaders();
+        responseHeaders.add("Location", location);
+        try {
+            exchange.sendResponseHeaders(302, -1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        exchange.close();
+    }
+
+    public void sendResponse(HttpExchange exchange, String response) {
+        byte[] bytes = response.getBytes();
+        try {
+            exchange.sendResponseHeaders(200, bytes.length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(bytes);
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendTemplateResponse(HttpExchange exchange, String templateName) {
+        JtwigTemplate template = JtwigTemplate.classpathTemplate(String.format("templates/%s.twig", templateName));
+        JtwigModel model = JtwigModel.newModel();
+        String response = template.render(model);
+        sendResponse(exchange, response);
+    }
 }
