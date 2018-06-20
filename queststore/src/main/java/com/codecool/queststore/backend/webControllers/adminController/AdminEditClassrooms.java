8815e45d6b9d20bbec02c32c718a7af7aef7aaa7
WojTechM <wojciech.m1997@gmail.com>
2018-06-20 18:48:56 +0200

    Create methods used to manage Classrooms

--- queststore/src/main/java/com/codecool/queststore/backend/webControllers/adminController/AdminEditClassrooms.java
+++ queststore/src/main/java/com/codecool/queststore/backend/webControllers/adminController/AdminEditClassrooms.java
@@ -8,12 +8,82 @@ import com.sun.net.httpserver.HttpHandler;
 import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
 
+import java.sql.SQLException;
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Map;
 
 public class AdminEditClassrooms extends AbstractHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) {
-        sendTemplateResponseClassrooms(exchange, "adminclassroom");
+        String[] uriParts = exchange.getRequestURI().toString().split("/");
+
+        if (uriParts.length == 3) {
+            // "/admin/classroom"
+            sendTemplateResponseClassrooms(exchange, "adminclassroom");
+        } else {
+
+            // "/admin/classroom/(action)"
+            int ACTION_I = 3;
+            String action = uriParts[ACTION_I];
+
+            if (action.equals("edit")) {
+                editClassroom(exchange);
+            } else if (action.equals("delete")) {
+                new AdminEditMentors().handle(exchange);
+            } else {
+                redirectToLocation(exchange, "/login");
+            }
+        }
+    }
+
+    private void editClassroom(HttpExchange exchange) {
+        String method = exchange.getRequestMethod();
+
+        if(method.equalsIgnoreCase("GET")) {
+            sendClassroomEditPage(exchange);
+        } else if (method.equalsIgnoreCase("POST")) {
+            submitClassroomEditPage(exchange);
+        } else {
+            redirectToLocation(exchange, "/admin/index");
+        }
+
+    }
+
+    private void sendClassroomEditPage(HttpExchange exchange) {
+        int classroomId = getClassId(exchange);
+        try {
+            Classroom classroom = new ClassroomDAO().loadClassroom(classroomId);
+            sendTemplateResponseClassEdit(exchange, "adminclassedit", classroom);
+        } catch (SQLException e) {
+            // Error occurred in database
+            redirectToLocation(exchange, "/admin");
+        } catch (NullPointerException e) {
+            // TODO
+            // Classroom not found
+            redirectToLocation(exchange, "/admin/classroom");
+        } catch (NumberFormatException | InputMismatchException e) {
+            // TODO
+            // Invalid input in browser (URI)
+            redirectToLocation(exchange, "/admin/classroom");
+        }
+    }
+
+    private void submitClassroomEditPage(HttpExchange exchange) {
+        try {
+            Map<String, String> inputs = readFormData(exchange);
+            int id = Integer.parseInt(exchange.getRequestURI().toString().split("/")[4]);
+            String name = inputs.get("name");
+            String description = inputs.get("description");
+            ClassroomDAO dao = new ClassroomDAO();
+            Classroom classroom = dao.loadClassroom(id);
+            classroom.setName(name);
+            classroom.setDescription(description);
+            dao.updateClassroom(classroom);
+            redirectToLocation(exchange, "/admin/classroom");
+        } catch (SQLException e) {
+            redirectToLocation(exchange, "/admin/index");
+        }
     }
 
     private void sendTemplateResponseClassrooms(HttpExchange exchange, String templateName) {
@@ -23,5 +93,25 @@ public class AdminEditClassrooms extends AbstractHandler implements HttpHandler 
         model.with("classrooms", classrooms);
         String response = template.render(model);
         sendResponse(exchange, response);
+    }
+
+    private void sendTemplateResponseClassEdit(HttpExchange exchange, String templateName, Classroom classroom) {
+
+        JtwigTemplate template = JtwigTemplate.classpathTemplate(String.format("templates/%s.jtwig", templateName));
+        JtwigModel model = JtwigModel.newModel();
+        model.with("id", classroom.getID());
+        model.with("name", classroom.getName());
+        model.with("description", classroom.getDescription());
+        String response = template.render(model);
+        sendResponse(exchange, response);
+    }
+
+    private int getClassId(HttpExchange exchange) {
+        try {
+            int id = Integer.parseInt(exchange.getRequestURI().toString().split("/")[4]);
+            return id;
+        } catch (IndexOutOfBoundsException e) {
+            return -1;
+        }
     }
 }
