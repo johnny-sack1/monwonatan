3a09b985891580166755294e065773524eba4a87
WojTechM <wojciech.m1997@gmail.com>
2018-06-20 10:20:38 +0200

    Create empty (TODO) HttpHandlers

---
+++ queststore/src/main/java/com/codecool/queststore/backend/webControllers/adminController/AClassroomPage.java
@@ -0,0 +1,14 @@
+package com.codecool.queststore.backend.webControllers.adminController;
+
+import com.codecool.queststore.backend.webControllers.AbstractHandler;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+public class AClassroomPage extends AbstractHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) {
+
+    }
+}
