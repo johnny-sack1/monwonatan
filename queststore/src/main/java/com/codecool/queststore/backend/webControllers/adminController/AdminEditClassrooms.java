c1dc8740d9bbad4bbcc54f93c0445cbb56f308e5
WojTechM <wojciech.m1997@gmail.com>
2018-06-20 13:51:46 +0200

    Create custom HttpHandler for classrooms management

--- queststore/src/main/java/com/codecool/queststore/backend/webControllers/adminController/AdminEditClassrooms.java
+++ queststore/src/main/java/com/codecool/queststore/backend/webControllers/adminController/AdminEditClassrooms.java
@@ -1,13 +1,27 @@
 package com.codecool.queststore.backend.webControllers.adminController;
 
+import com.codecool.queststore.backend.dao.ClassroomDAO;
+import com.codecool.queststore.backend.model.Classroom;
 import com.codecool.queststore.backend.webControllers.AbstractHandler;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
 
+import java.util.List;
 
 public class AdminEditClassrooms extends AbstractHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) {
+        sendTemplateResponseClassrooms(exchange, "adminclassroom");
+    }
 
+    private void sendTemplateResponseClassrooms(HttpExchange exchange, String templateName) {
+        List<Classroom> classrooms = new ClassroomDAO().loadAllClassrooms();
+        JtwigTemplate template = JtwigTemplate.classpathTemplate(String.format("templates/%s.jtwig", templateName));
+        JtwigModel model = JtwigModel.newModel();
+        model.with("classrooms", classrooms);
+        String response = template.render(model);
+        sendResponse(exchange, response);
     }
 }
