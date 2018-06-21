3b033cc2f63fcf0fb24d4411282502536e1838f5
reggae-krk <monika.gorecka.agh@gmail.com>
2018-06-21 12:01:17 +0200

    Add method responsible for hendling input form

--- queststore/src/main/java/com/codecool/queststore/backend/webControllers/studentController/StudentStore.java
+++ queststore/src/main/java/com/codecool/queststore/backend/webControllers/studentController/StudentStore.java
@@ -1,8 +1,10 @@
 package com.codecool.queststore.backend.webControllers.studentController;
 
+import com.codecool.queststore.backend.dao.BackpackDAO;
 import com.codecool.queststore.backend.dao.StudentDAO;
 import com.codecool.queststore.backend.dao.ArtifactDAO;
 import com.codecool.queststore.backend.model.Artifact;
+import com.codecool.queststore.backend.model.Backpack;
 import com.codecool.queststore.backend.model.Student;
 import com.codecool.queststore.backend.webControllers.AbstractHandler;
 import com.sun.net.httpserver.HttpExchange;
@@ -12,17 +14,23 @@ import org.jtwig.JtwigTemplate;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 public class StudentStore extends AbstractHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) {
+
         if(exchange.getRequestMethod().equals("GET")) {
             sendTemplateResponseStore(exchange, "studentStore",
                     getSidFromCookieStr(exchange.getRequestHeaders().getFirst("Cookie")));
         }
         else if (exchange.getRequestMethod().equals("POST")) {
-            //TODO student click buy button
+            Map inputs = readFormData(exchange);
+            String artifactName = (String) inputs.get("name");
+            System.out.println(artifactName);
+            sendTemplateResponseStore(exchange, "studentStore",
+                    getSidFromCookieStr(exchange.getRequestHeaders().getFirst("Cookie")), artifactName);
         }
     }
 
@@ -46,6 +54,44 @@ public class StudentStore extends AbstractHandler implements HttpHandler {
         catch (SQLException e) {
             redirectToLocation(exchange, "login");
         }
+    }
 
+    public void sendTemplateResponseStore(HttpExchange exchange, String templateName, String sessionId, String artifactName) {
+
+        try {
+            String login = getSessionIdContainer().getUserLogin(sessionId);
+            StudentDAO studentDAO = new StudentDAO();
+            BackpackDAO backpackDAO = new BackpackDAO();
+            Student student = studentDAO.loadStudent(login);
+            ArtifactDAO artifactDAO = new ArtifactDAO();
+            Artifact artifact = artifactDAO.loadArtifact(artifactName);
+            int price = artifact.getPrice();
+
+            if (price < student.getCoolcoins()) {
+                student.substractCoins(price);
+                Backpack backpack = student.getBackpack();
+
+                backpack.addToBackpack(artifact, "unused");
+                System.out.println("aaaaa" + backpack.getStudentBackpack());
+                backpackDAO.updateBackpack(backpack);
+                System.out.println("rgggergerg" + backpack.getStudentBackpack());
+            }
+
+            studentDAO.updateStudent(student);
+
+            JtwigTemplate template = JtwigTemplate.classpathTemplate(String.format("templates/%s.jtwig", templateName));
+            JtwigModel model = JtwigModel.newModel();
+
+            List<Artifact> artifactList = new ArtifactDAO().loadAllArtifacts();
+
+            model.with("title", "Student store");
+            model.with("items", artifactList);
+            model.with("coins", student.getCoolcoins());
+            String response = template.render(model);
+            sendResponse(exchange, response);
+        }
+        catch (SQLException e) {
+            redirectToLocation(exchange, "login");
+        }
     }
 }
