98fcfc7dfa3226f562308739ea25c3f9fc687c49
reggae-krk <monika.gorecka.agh@gmail.com>
2018-06-25 16:46:04 +0200

    Add loadStudentBySessionID method

--- queststore/src/main/java/com/codecool/queststore/backend/webControllers/studentController/StudentProfile.java
+++ queststore/src/main/java/com/codecool/queststore/backend/webControllers/studentController/StudentProfile.java
@@ -1,6 +1,8 @@
-package com.codecool.queststore.backend.webControllers.studentController;
+package com.codecool.queststore.backend.webControllers.adminController;
+
 
 import com.codecool.queststore.backend.dao.StudentDAO;
+import com.codecool.queststore.backend.loginManager.PasswordManager;
 import com.codecool.queststore.backend.model.Student;
 import com.codecool.queststore.backend.webControllers.AbstractHandler;
 import com.sun.net.httpserver.HttpExchange;
@@ -8,67 +10,60 @@ import com.sun.net.httpserver.HttpHandler;
 import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Map;
+
 
 public class StudentProfile extends AbstractHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) {
-        if(exchange.getRequestMethod().equals("GET")) {
+        String method = exchange.getRequestMethod();
+        if (method.equalsIgnoreCase("GET")) {
             sendTemplateResponseProfile(exchange, "studentProfile",
                     getSidFromCookieStr(exchange.getRequestHeaders().getFirst("Cookie")));
         }
-        else if (exchange.getRequestMethod().equals("POST")) {
-            Map inputs = readFormData(exchange);
-            String firstName = (String) inputs.get("firstName");
-            String lastName = (String) inputs.get("lastName");
-            sendTemplateResponseProfile(exchange, "studentProfile",
-                    getSidFromCookieStr(exchange.getRequestHeaders().getFirst("Cookie")), firstName, lastName);
+
+        if (method.equalsIgnoreCase("POST")) {
+            try {
+                Student student = loadStudentBySessionID(getSidFromCookieStr(exchange.getRequestHeaders().getFirst("Cookie")));
+                updateStudent(student, exchange);
+            } catch (SQLException e) {
+                redirectToLocation(exchange, "/student");
+            }
         }
     }
 
-    //TODO if user wants to change password
-
-    public void sendTemplateResponseProfile(HttpExchange exchange, String templateName, String sessionId) {
+    private void sendTemplateResponseProfile(HttpExchange exchange, String templateName, String sessionId) {
         try {
-            String login = getSessionIdContainer().getUserLogin(sessionId);
-            Student student = new StudentDAO().loadStudent(login);
+            Student student = loadStudentBySessionID(sessionId);
             JtwigTemplate template = JtwigTemplate.classpathTemplate(String.format("templates/%s.jtwig", templateName));
             JtwigModel model = JtwigModel.newModel();
+
             model.with("title", "Student profile");
             model.with("coins", student.getCoolcoins());
             model.with("expLevel", student.getExpLvl().getDescription());
-            model.with("firstName", student.getFirstName());
-            model.with("lastName", student.getLastName());
+            model.with("firstname", student.getFirstName());
+            model.with("lastname", student.getLastName());
+
             String response = template.render(model);
             sendResponse(exchange, response);
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             redirectToLocation(exchange, "login");
         }
     }
 
-    public void sendTemplateResponseProfile(HttpExchange exchange, String templateName, String sessionId, String firstName, String lastName) {
-        try {
-            StudentDAO studentDAO = new StudentDAO();
-            String login = getSessionIdContainer().getUserLogin(sessionId);
-            Student student = studentDAO.loadStudent(login);
-            student.setFirstName(firstName);
-            student.setLastName(lastName);
-            studentDAO.updateStudent(student);
-            JtwigTemplate template = JtwigTemplate.classpathTemplate(String.format("templates/%s.jtwig", templateName));
-            JtwigModel model = JtwigModel.newModel();
-            model.with("title", "Student profile");
-            model.with("coins", student.getCoolcoins());
-            model.with("expLevel", student.getExpLvl().getDescription());
-            model.with("firstName", firstName);
-            model.with("lastName", lastName);
-            String response = template.render(model);
-            sendResponse(exchange, response);
-        }
-        catch (SQLException e) {
-            redirectToLocation(exchange, "login");
-        }
+    private Student loadStudentBySessionID(String sessionId) throws SQLException{
+        String login = getSessionIdContainer().getUserLogin(sessionId);
+        Student student = new StudentDAO().loadStudent(login);
+        return student;
     }
 }
