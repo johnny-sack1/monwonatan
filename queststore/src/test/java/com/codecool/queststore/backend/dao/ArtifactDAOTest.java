6858bee92f23f74607042a8220d54013a5c2f059
saturator22 <f.szczeski@gmail.com>
2018-07-04 18:28:44 +0200

    Add ArtifactDAOTest class with bug connected with PowerMock in test method testIsLoadingArtifacts

---
+++ queststore/src/test/java/com/codecool/queststore/backend/dao/ArtifactDAOTest.java
@@ -0,0 +1,76 @@
+package com.codecool.queststore.backend.dao;
+
+import com.codecool.queststore.backend.databaseConnection.SQLQueryHandler;
+import com.codecool.queststore.backend.model.Artifact;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Spy;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.spy;
+
+
+//@RunWith(PowerMockRunner.class)
+//@PrepareForTest(SQLQueryHandler.class)
+class ArtifactDAOTest {
+
+    ArtifactDAO artifactDAO;
+
+    @BeforeEach
+    void setupDAO() {
+        artifactDAO = new ArtifactDAO();
+    }
+
+    @Test
+    void testIsLoadingArtifact() throws SQLException {
+//        SQLQueryHandlerWrapper sqlQueryHandlerWrapper = spy(new SQLQueryHandlerWrapper());
+        Artifact testArtifact = new Artifact(1, true,
+                "skryptPHP", "nvm", 300);
+        String expectedQuery = "SELECT * FROM artifact WHERE artifact_id = ?";
+        SQLQueryHandler spySQL = spy();
+        SQLQueryHandler mockedSQLQueryHandler = mock(SQLQueryHandler.class);
+        Connection mockedConnection = mock(Connection.class);
+        ResultSet mockedResultSet = mock(ResultSet.class);
+        ArgumentCaptor<String> argument = ArgumentCaptor.forClass(String.class);
+
+        //todo labels do zmiennych, argument value do zmiennej
+//        mockStatic(SQLQueryHandler.class);
+        when(SQLQueryHandler.getInstance()).thenReturn(mockedSQLQueryHandler);
+//        doReturn(mockedSQLQueryHandler).when(sqlQueryHandlerWrapper).getInstance();
+        when(mockedSQLQueryHandler.getConnection()).thenReturn(mockedConnection);
+        when(mockedSQLQueryHandler.executeQuery(anyString())).thenReturn(mockedResultSet);
+        when(mockedResultSet.next()).thenReturn(true);
+        when(mockedResultSet.getBoolean("available_for_groups")).thenReturn(true);
+        when(mockedResultSet.getString("name")).thenReturn("skryptPHP");
+        when(mockedResultSet.getString("description")).thenReturn("nvm");
+        when(mockedResultSet.getInt("price")).thenReturn(300);
+
+        artifactDAO.loadArtifact(anyInt());
+
+        verify(mockedConnection).prepareStatement(argument.capture());
+        assertEquals(expectedQuery, argument.getValue());
+
+    }
+
+    @Test
+    void testIsUpdatingArtifact() {
+    }
+
+    @Test
+    void testIsLoadingAllArtifacts() {
+    }
+}
